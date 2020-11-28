0e411b82e79b7acabf43799d45d6fee5ea2abb53
MuhammadBayu66 <saputrabayuu283@gmail.com>
2020-11-28 13:42:51 +0700

    menambahkan login manual serta koneksi database & seeder

--- resources/views/Template/left-sidebar.blade.php
+++ resources/views/Template/left-sidebar.blade.php
@@ -70,6 +70,12 @@
               </li>
             </ul>
           </li>
+          <li class="nav-item">
+                <a href="{{ route('logout') }}" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                     Logout
+                <p>
          
         </ul>
       </nav>
